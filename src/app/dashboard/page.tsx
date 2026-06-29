39724901fbf9bb74e55c2f3cf985a02016a9a27a
Yungtimmy <140066822+Yungtimmy@users.noreply.github.com>
2026-06-29 22:36:55 +0100

    Add PointsTicker component to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -108,9 +108,9 @@ export default async function DashboardPage() {
         </div>
       </div>
 
-      <PointsTicker />  {/* 👈 added here */}
+      <PointsTicker />
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {/* Main: matches */}
         <div className="lg:col-span-2 space-y-4">
           {/* Live */}
